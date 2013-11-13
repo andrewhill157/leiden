1979e2f28ac769a6d3ba1d7abf1bdbafd0ccd3d2
Andrew Hill <andrewhill157@gmail.com>
2013-11-12 20:05:30 -0500

    Moved files to different directory. Bug fix: corrected bug where DNA change column was not being correctly outputted to MutalizerInput file. This was a result of inconsistent column order between databases. Column headers are now used to determine the correct column.

--- data/1 - Raw Leiden Data (Some Pre-Processing)/__init__.py
+++ data/1 - Raw Leiden Data (Some Pre-Processing)/__init__.py
@@ -6,6 +6,22 @@ FILE_EXTENSION = '.txt'
 ROW_DELIMITER = '\n'
 COLUMN_DELIMITER = ','
 
+"""
+Given a list of strings and a string to search for, returns the first index of the first element in the list that contains the search string.
+Note that the comparison is not sensitive to case or leading or trailing whitespace characters.
+@params stringList - list of strings
+@params searchString - a string to search for in elements of stringList
+@returns - index of the first instance of the search string in an element of the list. Returns -1 if the search string is not found.
+"""
+def findStringIndex(stringList, searchString):
+	i = 0
+	for entry in stringList:
+		if searchString.upper().strip() in entry.upper().strip():
+			return i
+		else: 
+			i = i + 1
+	return -1
+
 def saveGeneData(geneID):
 	database = LeidenDatabase(geneID)
 	fileName = "".join([geneID, FILE_EXTENSION])
@@ -16,15 +32,18 @@ def saveGeneData(geneID):
 		entries = database.getTableData()
 		fileLines = []
 		headers = database.getTableHeaders()
+
 		fileLines.append(COLUMN_DELIMITER.join(headers))
-						 
+		
+		HVGSMutationColumn = findStringIndex(headers, u'DNA\xa0change')
+
 		for rows in entries:
 			fileLines.append(COLUMN_DELIMITER.join(rows))
-			mutalizer.write("".join([rows[1].encode("UTF-8"), "\n"]))
+			mutalizer.write("".join([rows[HVGSMutationColumn].encode("UTF-8"), "\n"]))
 		f.write(ROW_DELIMITER.join(fileLines).encode("UTF-8"))
 		
 """
-Document script usage
+TODO document
 """
 arguments = len(sys.argv)
 if arguments > 2:
