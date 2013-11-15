b577efff986db602ab438ea01f7db5632755120c
Andrew Hill <andrewhill157@gmail.com>
2013-11-14 21:26:08 -0500

    Added improved command line interface to allow processing of all files in the directory or a list of specified files.

--- data/4 - Annotations/processAnnotatedMutations.py
+++ data/4 - Annotations/processAnnotatedMutations.py
@@ -1,6 +1,6 @@
 import re
 import sys
- 
+import glob
  
 def mapAACodes(code):
 	# Mapping from three letter amino acid codes to one letter amino acid codes 
@@ -102,14 +102,80 @@ def getAlleleFrequency(annotationInfo):
 		AC = float(AC)
 		AN = float(AN)
 		return (AC/AN)*100
+
+def flagAnnotationFile(annotationFile):
+	# Do not process any flagged files in the directory
+	if "FLAGGED_" not in annotationFile:
+		resultsFile = "".join(["FLAGGED_", annotationFile])
+
+		with open(annotationFile) as entries, open(resultsFile, 'w') as results:
+			for line in entries:
+				# Only process flags for lines that are not header information
+				if "#" not in line:
+					flags = []
+					# STEP 1 - Check that LAA_CHANGE= and AA_CHANGE= entries are the same (that entries are concordant)
+					annotationInfo = getAnnotationInfo(line)
+					LAA_CHANGE = getLAAChange(annotationInfo)
+					AA_CHANGE = getAAChange(annotationInfo)
+					
+					CONCORDANCE_FLAG = "CONCORDANCE="
+					if LAA_CHANGE is "" or AA_CHANGE is "":
+						flags.append("".join([CONCORDANCE_FLAG, "NO_AA_CHANGE"]))
+						
+					else:
+						concordance = isConcordant(LAA_CHANGE, AA_CHANGE)
+						if concordance == "OK":
+							flags.append("".join([CONCORDANCE_FLAG, "CONCORDANT"]))
+						elif concordance == "FAIL":
+							flags.append("".join([CONCORDANCE_FLAG,"NOT_CONCORDANT"]))
+						elif concordance == "ERROR":
+							flags.append("".join([CONCORDANCE_FLAG,"ERROR"]))	
+					
+					# STEP 2 - Extract the SEVERE_IMPACT entry
+					SEVERE_IMPACT_FLAG = "SEVERE_IMPACT="
+					severeImpact = getSevereImpact(annotationInfo)
+					flags.append("".join([SEVERE_IMPACT_FLAG,severeImpact]))
+						
+					# STEP 3 - Check for overlap with HGMD
+					HGMD_FLAG = "HGMD="
+					if "HGMD_MUT" not in line:
+						flags.append("".join([HGMD_FLAG,"NOT_IN_HGMD"]))
+					else: 
+						flags.append("".join([HGMD_FLAG,"OVERLAPS"]))
+							
+					# Step 4 - Check for allele frequency information (above or below overall 0.5% frequency
+					ALLELE_FREQUENCY_TAG = "ALLELE_FREQUENCY="
+					alleleFrequency = getAlleleFrequency(annotationInfo)
+					if alleleFrequency == "":
+						flags.append("".join([ALLELE_FREQUENCY_TAG,"NOT_IN_26K_DATABASE"]))
+					elif alleleFrequency > 0.5:
+						flags.append("".join([ALLELE_FREQUENCY_TAG,"HIGH_FREQUENCY"]))
+					else: 
+						flags.append("".join([ALLELE_FREQUENCY_TAG,"LOW_FREQUENCY"]))
+						
+					# Write out lines to new file with [LIST_OF_FLAGS] inserted as the new first column
+					results.write(";".join(flags))
+					results.write("\t")
+					results.write(line)
+				
+				# Copy header data unchanged from original file
+				elif line.startswith("##"):
+					results.write(line)
+					
+				# Copy column labels with new column, FLAGS, inserted as label for first column
+				elif line.startswith("#"):
+					results.write("".join([line[0], "FLAGS", "\t", line[1:]]))
 		
 """
-This script must be called in the following format:
-	python processAnnotatedMutations.py myAnnotatedMutations.vcf
-Note that the .vcf file parameter should be adjusted to the file name of the file to be processed.
+This script can be called in two ways:
+	1. python processAnnotatedMutations.py myAnnotatedMutations.vcf myAnnotatedMutations2.vcf ...
+	2. python processAnnotatedMutations.py
 
-The script will output a file in the same directory as the script called FLAGGED_<original_file_name>.vcf
-For example, the command shown in the paragraph above would produce a file called FLAGGED_myAnnotatedMutations.vcf
+The first option will process all files specified as command line parameters. The second option will process all 
+.vcf files in the current directory (files with FLAGGED_ in the name will be ignored).
+
+The script will output files in the same directory as the script called FLAGGED_<original_file_name>.vcf
+For example, the command shown in number one above would produce files called FLAGGED_myAnnotatedMutations.vcf, etc.
 
 The output file will contain all header same information as the original file, except the column will now contain a semi-colon delimited list of 
 tags and corresponding values for each mutation: TAG1=VALUE;TAG2=VALUE;TAG3=VALUE... 
@@ -134,66 +200,20 @@ ALLELE_FREQUENCY=
 	- HIGH_FREQUENCY: Overall allele frequency,(AC_MAC26K/AN_MAC26K)*100, is greater than 0.5%
 	- NOT_IN_26K_DATABASE: There were no AC_MAC26K or AN_MAC26K entries for this mutation
 """
-if len(sys.argv) is not 2:
-	raise Exception("Must use one input argument")
+arguments = len(sys.argv)
+if arguments > 1:
+	for files in sys.argv[1:]:
+		try:
+			flagAnnotationFile(files)
+		except: 
+			print ": ".join(["Error Processing File", files])
+else:
+	for files in glob.glob("*.vcf"):
+		try:
+			flagAnnotationFile(files)
+		except: 
+			print ": ".join(["Error Processing File", files])
 	
-annotationFile = str(sys.argv[1])
-resultsFile = "".join(["FLAGGED_", annotationFile])
-with open(annotationFile) as entries, open(resultsFile, 'w') as results:
-	for line in entries:
-		# Only process flags for lines that are not header information
-		if "#" not in line:
-			flags = []
-			# STEP 1 - Check that LAA_CHANGE= and AA_CHANGE= entries are the same (that entries are concordant)
-			annotationInfo = getAnnotationInfo(line)
-			LAA_CHANGE = getLAAChange(annotationInfo)
-			AA_CHANGE = getAAChange(annotationInfo)
-			
-			CONCORDANCE_FLAG = "CONCORDANCE="
-			if LAA_CHANGE is "" or AA_CHANGE is "":
-				flags.append("".join([CONCORDANCE_FLAG, "NO_AA_CHANGE"]))
-				
-			else:
-				concordance = isConcordant(LAA_CHANGE, AA_CHANGE)
-				if concordance == "OK":
-					flags.append("".join([CONCORDANCE_FLAG, "CONCORDANT"]))
-				elif concordance == "FAIL":
-					flags.append("".join([CONCORDANCE_FLAG,"NOT_CONCORDANT"]))
-				elif concordance == "ERROR":
-					flags.append("".join([CONCORDANCE_FLAG,"ERROR"]))	
-			
-			# STEP 2 - Extract the SEVERE_IMPACT entry
-			SEVERE_IMPACT_FLAG = "SEVERE_IMPACT="
-			severeImpact = getSevereImpact(annotationInfo)
-			flags.append("".join([SEVERE_IMPACT_FLAG,severeImpact]))
-				
-			# STEP 3 - Check for overlap with HGMD
-			HGMD_FLAG = "HGMD="
-			if "HGMD_MUT" not in line:
-				flags.append("".join([HGMD_FLAG,"NOT_IN_HGMD"]))
-			else: 
-				flags.append("".join([HGMD_FLAG,"OVERLAPS"]))
-					
-			# Step 4 - Check for allele frequency information (above or below overall 0.5% frequency
-			ALLELE_FREQUENCY_TAG = "ALLELE_FREQUENCY="
-			alleleFrequency = getAlleleFrequency(annotationInfo)
-			if alleleFrequency == "":
-				flags.append("".join([ALLELE_FREQUENCY_TAG,"NOT_IN_26K_DATABASE"]))
-			elif alleleFrequency > 0.5:
-				flags.append("".join([ALLELE_FREQUENCY_TAG,"HIGH_FREQUENCY"]))
-			else: 
-				flags.append("".join([ALLELE_FREQUENCY_TAG,"LOW_FREQUENCY"]))
-				
-			# Write out lines to new file with [LIST_OF_FLAGS] inserted as the new first column
-			results.write(";".join(flags))
-			results.write("\t")
-			results.write(line)
-		
-		# Copy header data unchanged from original file
-		elif line.startswith("##"):
-			results.write(line)
-			
-		# Copy column labels with new column, FLAGS, inserted as label for first column
-		elif line.startswith("#"):
-			results.write("".join([line[0], "FLAGS", "\t", line[1:]]))
+
+
 			
