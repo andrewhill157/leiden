faeab4994daaa7da3a3671f104fa2ade8e6b94ca
Andrew Hill <andrewhill157@gmail.com>
2013-11-16 10:12:19 -0500

    Improved documentation.

--- 3 - Open Refine Output/compileMappings.py
+++ 3 - Open Refine Output/compileMappings.py
@@ -31,14 +31,16 @@ def combineLeidenMutalyzer(rawLeidenDataFile, mutalyzerOutputFile):
 
 		return combinedData
 
-"""
-Given a list of strings and a string to search for, returns the first index of the first element in the list that contains the search string.
-Note that the comparison is case sensitive and the element at a given index must only contain the search element, not exactly match the search element.
-@params stringList - list of strings
-@params searchString - a string to search for in elements of stringList
-@returns - index of the first instance of the search string in an element of the list. Returns -1 if the search string is not found.
-"""
+
 def findStringIndex(stringList, searchString):
+	"""
+	Given a list of strings and a string to search for, returns the first index of the first element in the list that contains the search string.
+	Note that the comparison is case sensitive and the element at a given index must only contain the search element, not exactly match the search element.
+	@param stringList: list of strings
+	@param searchString: a string to search for in elements of stringList
+	@rtype: number
+	@returns: index of the first instance of the search string in an element of the list. Returns -1 if the search string is not found.
+	"""
 	i = 0
 	for entry in stringList:
 		if searchString in entry:
@@ -47,58 +49,67 @@ def findStringIndex(stringList, searchString):
 			i = i + 1
 	return -1
 
-"""
-Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
-returns tha chromosome number from the mutation mapping. For example, calling on NC_000001.2:g.524264A>C 
-would return 1. 
-The mapping is assumed to be in valid HVGS notation.
-@params mapping - string with the mapping of a mutation in HVGS notation as shown above.
-@returns - chromosome number of the mutation as a string
-"""
+
 def getChromosomeNumber(mapping):
+	"""
+	Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
+	returns tha chromosome number from the mutation mapping. For example, calling on NC_000001.2:g.524264A>C 
+	would return 1. 
+	The mapping is assumed to be in valid HVGS notation.
+	@param mapping: string with the mapping of a mutation in HVGS notation as shown above.
+	@rtype: string
+	@returns: chromosome number of the mutation as a string
+	"""
 	m = re.search('([0]+)([1-9][0-9]?)([.])', mapping)
 	return m.group(2)
 
-"""
-Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
-returns tha coordinate from the mutation mapping. For example, calling on NC_0000001.2:g.524264A>C 
-would return 524264. 
-The mapping is assumed to be in valid HVGS notation.
-@params mapping - string with the mapping of a mutation in HVGS notation as shown above.
-@returns - coordinates of the mutation as a string
-"""
+
 def getCoordinates(mapping):
+	"""
+	Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
+	returns tha coordinate from the mutation mapping. For example, calling on NC_0000001.2:g.524264A>C 
+	would return 524264. 
+	The mapping is assumed to be in valid HVGS notation.
+	@param mapping: string with the mapping of a mutation in HVGS notation as shown above.
+	@rtype: string
+	@returns: coordinates of the mutation as a string
+	"""
 	m = re.search('([g][.])([0-9]+[_]?[0-9]+)', mapping)
 	return m.group(2)
 
-"""
-Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
-returns tha REF (reference) base from the mutation mapping. For example, calling on NC_0000001:g.524264A>C 
-would return A. 
-The mapping is assumed to be in valid HVGS notation.
-@params mapping - string with the mapping of a mutation in HVGS notation as shown above.
-@returns - reference base of the mutation (base before the mutation) as a string
-"""
+
 def getRef(mapping):
+	"""
+	Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
+	returns tha REF (reference) base from the mutation mapping. For example, calling on NC_0000001:g.524264A>C 
+	would return A. 
+	The mapping is assumed to be in valid HVGS notation.
+	@param mapping: string with the mapping of a mutation in HVGS notation as shown above.
+	@rtype: string
+	@returns: reference base of the mutation (base before the mutation) as a string
+	"""
 	m = re.search('([A-Z])([>])([A-Z])', mapping)
 	return m.group(1)
 
-"""
-Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
-returns tha ALT (reference) base from the mutation mapping. For example, calling on NC_0000001:g.524264A>C 
-would return C. 
-The mapping is assumed to be in valid HVGS notation.
-@params mapping - string with the mapping of a mutation in HVGS notation as shown above.
-@returns - alternate base of the mutation (base after the mutation) as a string
-"""
+	
 def getAlt(mapping):
+	"""
+	Given a mapping of the form NC_######.#:g.<HVGS notation>, where NC_###### is the chromosome reference ID, 
+	returns tha ALT (reference) base from the mutation mapping. For example, calling on NC_0000001:g.524264A>C 
+	would return C. 
+	The mapping is assumed to be in valid HVGS notation.
+	@param mapping: string with the mapping of a mutation in HVGS notation as shown above.
+	@rtype: string
+	@returns: alternate base of the mutation (base after the mutation) as a string
+	"""
 	m = re.search('([A-Z])([>])([A-Z])', mapping)
 	return m.group(3)
 
-"""
-TODO document
-"""
+
 def convertToVCF(combinedData):
+	"""
+	TODO document
+	"""
 	mappingIndex = findStringIndex(combinedData[0], "Chromosomal Variant")
 	VCFMapping = []
 	combinedData[0].insert(mappingIndex, "ALT")
@@ -136,42 +147,42 @@ def convertToVCF(combinedData):
 
 	return VCFMapping
 
-"""
-Writes a list of lists to a file with a specified file name. 
-Elements of listOfLists (lists themselves) are each written to their own line in the file with a tab character separating each entry.
-@params listOfLists - list whose elements are lists of data elements such as strings, ints, etc. 
-@params fileName - File name of the file to write listOfLists to. Must be a valid file name with extension.
-"""
 def writeListOfListsToFile(listOfLists, fileName):
+	"""
+	Writes a list of lists to a file with a specified file name. 
+	Elements of listOfLists (lists themselves) are each written to their own line in the file with a tab character separating each entry.
+	@param listOfLists: list whose elements are lists of data elements such as strings, ints, etc. 
+	@param fileName: File name of the file to write listOfLists to. Must be a valid file name with extension.
+	"""
 	with open(fileName, 'w') as f:
 		for lists in listOfLists:
 			f.write("\t".join(lists))
 
-"""
-Removes the file extension from a given file. 
-@params fileName - name of the file whose extension is to be removed. fileName may not contain any other "." characters other than that that
-preceeds the file extension. Files without extensions are returned unmodified.
-@returns fileName with no file extension ("." character is also removed). For example an input of myFile.txt would return a new string, myFile
-"""
+
 def removeFileExtension(fileName):
-	extensionIndex = fileName.find(".")
-	return fileName[0:extensionIndex]
+	"""
+	Removes the file extension from a given file. 
+	@param fileName: name of the file whose extension is to be removed. Files without extensions are returned unmodified.
+	@rtype: string
+	@returns: fileName with no file extension ("." character is also removed). For example an input of myFile.txt would return a new string, myFile
+	"""
+	return os.path.splitext(filename)[0]
 
-"""
-TODO document
-"""
+
 def combineMappingData(rawLeidenDataFile):
-	try:
-		if ".txt" in files and "MutalizerOutput" not in files and "_MAPPED" not in files:
-			rawLeidenDataFile = files
-			
-			mutalyzerOutputFile = "".join([removeFileExtension(files), "_MutalizerOutput.txt"])
+	"""
+	TODO document
+	"""
+	# Do not want to process MutalizerOutput files of files that have already been combined (_MAPPED)
+	if ".txt" in files and "MutalizerOutput" not in files and "_MAPPED" not in files:
+		rawLeidenDataFile = files
+		
+		mutalyzerOutputFile = "".join([removeFileExtension(files), "_MutalizerOutput.txt"])
 
-			combinedData = combineLeidenMutalyzer(rawLeidenDataFile, mutalyzerOutputFile)
-			VCFData = convertToVCF(combinedData)
-			writeListOfListsToFile(VCFData, "_".join([removeFileExtension(rawLeidenDataFile), "_MAPPED.txt"]))
-	except: 
-		print ": ".join(["Error processing gene", files])
+		combinedData = combineLeidenMutalyzer(rawLeidenDataFile, mutalyzerOutputFile)
+		VCFData = convertToVCF(combinedData)
+		writeListOfListsToFile(VCFData, "_".join([removeFileExtension(rawLeidenDataFile), "_MAPPED.txt"]))
+		
 
 """
 This script can be called in two ways:
