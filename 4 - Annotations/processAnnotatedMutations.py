da24feba84899e2b9bb9f7dfda888f1b447377d3
Andrew Hill <andrewhill157@gmail.com>
2013-11-20 10:00:59 -0500

    Output files in different format <original file name without _FINAL.vcf>_FLAGGED.vcf. Improved filtering of file names.

--- 4 - Annotations/processAnnotatedMutations.py
+++ 4 - Annotations/processAnnotatedMutations.py
@@ -193,8 +193,10 @@ def flag_annotation_file(annotation_file):
     TODO document
     @param annotation_file:
     """
+    flag_index = annotation_file.find("_")
 
-    results_file = "".join(["FLAGGED_", annotation_file])
+    # <original file up to the start of _FINAL.vcf>_FLAGGED.vcf
+    results_file = annotation_file[0:flag_index] + "_FLAGGED.vcf"
 
     with open(annotation_file) as entries, open(results_file, 'w') as results:
         for line in entries:
@@ -276,37 +278,38 @@ BEGIN SCRIPT
 
 parser = argparse.ArgumentParser(description="Given the VCF-like file output by annotation of variants, add a column \
     of flags as the first column in the file to summarize information contained in the INFO column. \
-    The script will output files in the same directory as the script called <original_file_name>_FLAGGED.vcf \n\
-For example, the command shown in number one above would produce files called FLAGGED_myAnnotatedMutations.vcf, etc. \
-\n\
-The output file will contain all the same information as the original file, except the column will now contain a \
-semi-colon delimited list of tags and corresponding values for each mutation: TAG1=VALUE;TAG2=VALUE;TAG3=VALUE \
-Note that the original columns in the file will all be shifted right one column in the output.\
-Tag and value definitions in resulting output file: \n\
-CONCORDANCE= \n\
-    - CONCORDANT: The amino acid changes predicted by LAA_CHANGE and AA_CHANGE in the input file match \n\
-    - NOT_CONCORDANT: The amino acid changes predicted by LAA_CHANGE and AA_CHANGE in the input file do not match \n\
-    - ERROR: There was an error processing the LAA_CHANGE and/or AA_CHANGE entry \n\
-    - NO_AA_CHANGE: No amino acid change is predicted by the annotation \n\
-\n\
-SEVERE_IMPACT= \n\
-    - The value of SEVERE_IMPACT in the original file is simply copied as the value of this tag for convenience \n\
-\n\
-HGMD= \n\
-    - OVERLAPS - An HGMD_MUT entry is present in the original file \n\
-    - NOT_IN_HGMD - No HGMD_MUT entry is present in the original file \n\
-\n\
-ALLELE_FREQUENCY=  \n\
-    - LOW_FREQUENCY: Overall allele frequency,(AC_MAC26K/AN_MAC26K)*100, is less than or equal to 0.5% \n\
-    - HIGH_FREQUENCY: Overall allele frequency,(AC_MAC26K/AN_MAC26K)*100, is greater than 0.5% \n\
-    - NOT_IN_26K_DATABASE: There were no AC_MAC26K or AN_MAC26K entries for this mutation")
+    The script will output files in the same directory as the script called <original_file_name>_FLAGGED.vcf \
+    For example, the command python processAnnotatedMutations ACTA1_FINAL.vcf would produce a file called \
+    ACTA1_FLAGGED.vcf, etc. \
+    \
+    The output file will contain all the same information as the original file, except the column will now contain a \
+    semi-colon delimited list of tags and corresponding values for each mutation: TAG1=VALUE;TAG2=VALUE;TAG3=VALUE \
+    Note that the original columns in the file will all be shifted right one column in the output.\
+    Tag and value definitions in resulting output file: \
+    CONCORDANCE= \
+        - CONCORDANT: The amino acid changes predicted by LAA_CHANGE and AA_CHANGE in the input file match \
+        - NOT_CONCORDANT: The amino acid changes predicted by LAA_CHANGE and AA_CHANGE in the input file do not match \
+        - ERROR: There was an error processing the LAA_CHANGE and/or AA_CHANGE entry \
+        - NO_AA_CHANGE: No amino acid change is predicted by the annotation \
+    \
+    SEVERE_IMPACT= \
+        - The value of SEVERE_IMPACT in the original file is simply copied as the value of this tag for convenience \
+    \
+    HGMD= \
+        - OVERLAPS - An HGMD_MUT entry is present in the original file \
+        - NOT_IN_HGMD - No HGMD_MUT entry is present in the original file \
+    \
+    ALLELE_FREQUENCY=  \
+        - LOW_FREQUENCY: Overall allele frequency,(AC_MAC26K/AN_MAC26K)*100, is less than or equal to 0.5% \
+        - HIGH_FREQUENCY: Overall allele frequency,(AC_MAC26K/AN_MAC26K)*100, is greater than 0.5% \
+        - NOT_IN_26K_DATABASE: There were no AC_MAC26K or AN_MAC26K entries for this mutation")
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-d", "--debug", action="store_true",
                    help="When errors are encountered, a full stack traceback is printed.")
 group.add_argument("-a", "--all", action="store_true",
-                   help="Process all vcf files in the given directory. There can be no other types of VCF files in the \
-                   directory except those containing FLAGGED in the name, which are ignored.")
+                   help="Process all vcf files in the given directory. Only files containing FINAL in the file name \
+                   will be processed.")
 parser.add_argument("file_name", help="File or files (including extension) to process.", nargs="*")
 
 args = parser.parse_args()
