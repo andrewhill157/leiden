f900afc5fe519ce1c5788b40f9389466c189c1bf
Andrew Hill <andrewhill157@gmail.com>
2014-02-04 19:45:31 -0500

    Updated automation scripts to match new directory structure on cluster.

--- Data_Extraction/uploadEye.sh
+++ Data_Extraction/uploadEye.sh
@@ -1,6 +1,6 @@
 # Move results to appropriate folders
 mv *.txt LOVD_Eye_Extracted_Data/
-mv *.vcf ../Validation/LOVD_Eye_VEP_Validation/
+mv *.vcf ../Validation/eye/
 
 # Transfer VCF files to server
-scp ../Validation/LOVD_Eye_VEP_Validation/*.vcf tin:/humgen/atgu1/fs03/ahill/Leiden_Database_Cleanup/eye/
+scp ../Validation/eye/*.vcf tin:/humgen/atgu1/fs03/ahill/Leiden_Database_Cleanup/eye/
--- Data_Extraction/uploadMuscle.sh
+++ Data_Extraction/uploadMuscle.sh
@@ -1,6 +1,6 @@
 # Move results to appropriate folders
 mv *.txt LOVD_Muscular_Dystrophy_Pages_Extracted_Data/
-mv *.vcf ../Validation/LOVD_Muscular_Dystrophy_Pages_VEP_Validation/
+mv *.vcf ../Validation/muscle/
 
 # Transfer VCF files to server
-scp ../Validation/LOVD_Muscular_Dystrophy_Pages_VEP_Validation/*.vcf tin:/humgen/atgu1/fs03/ahill/Leiden_Database_Cleanup/muscle/
+scp ../Validation/muscle/*.vcf tin:/humgen/atgu1/fs03/ahill/Leiden_Database_Cleanup/muscle/
