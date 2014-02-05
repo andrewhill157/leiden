d7efefa75ea2ba85cf1f0d8205f1f29fee60c650
Andrew Hill <andrewhill157@gmail.com>
2014-02-04 19:44:48 -0500

    Changed the LAA_CHANGE so it comes from the column that contains Protein, not Protein change. Some LOVD websites only have Protein as the label...

--- Data_Extraction/ExtractLeidenDataFunctions.py
+++ Data_Extraction/ExtractLeidenDataFunctions.py
@@ -86,7 +86,7 @@ def format_vcf_text(header, table_data, remapping):
     # Build remaining rows using remapped variants
     for i in range(0, len(remapping.chromosome_number)):
         # Extract amino-acid change
-        amino_acid_change_row = Utilities.find_string_index(header, 'Protein\xa0change')
+        amino_acid_change_row = Utilities.find_string_index(header, 'Protein')
         laa_change = table_data[i][amino_acid_change_row]
 
         row = [remapping.chromosome_number[i], remapping.coordinate[i], '.', remapping.ref[i], remapping.alt[i], '.', '.', 'LAA_CHANGE='+laa_change]
