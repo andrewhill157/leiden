7f5dd8583ba230f6346b1ceaa7abbe9d3ca02103
Andrew Hill <andrewhill157@gmail.com>
2014-02-19 12:21:22 -0500

    Slight modification to upload script to reflect path change on cluster.

--- Data_Extraction/uploadMuscle.sh
+++ Data_Extraction/uploadMuscle.sh
@@ -3,4 +3,5 @@ mv *.txt LOVD_Muscular_Dystrophy_Pages_Extracted_Data/
 mv *.vcf ../Validation/muscle/
 
 # Transfer VCF files to server
+ssh tin 'rm /humgen/atgu`/fs03/ahill/Leiden_Database_Cleanup/muscle/*'
 scp ../Validation/muscle/*.vcf tin:/humgen/atgu1/fs03/ahill/Leiden_Database_Cleanup/muscle/
